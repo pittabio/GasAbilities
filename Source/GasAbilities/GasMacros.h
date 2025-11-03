aa544cca5593e17525a770d98930bf8ef4d26d9e
Fabio Pittaccio <pittacciofabio@gmail.com>
2025-11-03 18:12:27 +0100

    feat: add debug macros for visual debugging in GasCharacter

---
+++ Source/GasAbilities/GasMacros.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#define PRINT(DebugMessage, ...) {GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT(DebugMessage), __VA_ARGS__)); }
+
+#define SPHERE(Center) { if (GetWorld()) DrawDebugSphere(GetWorld(), Center, 20.f, 16, FColor::Blue, true); }
+#define SPHERE_TICK(Center) { if (GetWorld()) DrawDebugSphere(GetWorld(), Center, 20.f, 16, FColor::Green, false); }
+
+#define LINE(Start, End) { if (GetWorld()) DrawDebugLine(GetWorld(), Start, End, FColor::Blue, true); }
+#define LINE_TICK(Start, End) { if (GetWorld()) DrawDebugLine(GetWorld(), Start, End, FColor::Green, false); }
