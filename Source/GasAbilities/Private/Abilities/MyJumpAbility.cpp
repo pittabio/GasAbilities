48dec92ffd197f72240e9522356e9b27c1fc6637
Fabio Pittaccio <pittacciofabio@gmail.com>
2025-11-05 12:12:10 +0100

    feat: add MyJumpAbility for character jump functionality in GAS

---
+++ Source/GasAbilities/Private/Abilities/MyJumpAbility.cpp
@@ -0,0 +1,19 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#include "Abilities/MyJumpAbility.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemComponent.h"
+
+void UMyJumpAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	// Check if the actor has a valid Character reference
+	if (ACharacter* MyCharacter = Cast<ACharacter>(ActorInfo->AvatarActor.Get()))
+		MyCharacter->Jump();
+
+	// End the ability immediately after performing the jump
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+}
---
+++ Source/GasAbilities/Public/Abilities/MyJumpAbility.h
@@ -0,0 +1,25 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "MyJumpAbility.generated.h"
+
+/**
+ * UMyJumpAbility is a basic Gameplay Ability that allows the owning Character to perform a jump.
+ * This ability is used to test the fundamental activation and execution flow of simple, 
+ * instant Abilities within the Gameplay Ability System (GAS).
+ */
+UCLASS()
+class GASABILITIES_API UMyJumpAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+
+	// Override the ActivateAbility function to implement the jump logic
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	
+};
