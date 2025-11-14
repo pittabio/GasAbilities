4646fc58e5884233c92aacc11f5b65ab849656aa
Fabio Pittaccio <pittacciofabio@gmail.com>
2025-11-14 12:49:08 +0100

    feat: initialize player character

---
+++ Source/GasAbilities/Private/02_Advanced/Kai/Characters/KaiPlayerCharacter.cpp
@@ -0,0 +1,35 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#include "02_Advanced/Kai/Characters/KaiPlayerCharacter.h"
+#include "02_Advanced/Kai/AbilitySystem/KaiAbilitySystemComponent.h"
+#include "02_Advanced/Kai/Player/KaiPlayerState.h"
+
+// Sets default values
+AKaiPlayerCharacter::AKaiPlayerCharacter()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AKaiPlayerCharacter::InitAbilitySystemComponent()
+{
+	AKaiPlayerState* KaiPlayerState = GetPlayerState<AKaiPlayerState>();
+	check(KaiPlayerState);
+	
+	AbilitySystemComponent = CastChecked<UKaiAbilitySystemComponent>(KaiPlayerState->GetAbilitySystemComponent());
+	AbilitySystemComponent->InitAbilityActorInfo(KaiPlayerState, this);
+}
+
+void AKaiPlayerCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+	
+	InitAbilitySystemComponent();
+}
+
+void AKaiPlayerCharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+	
+	InitAbilitySystemComponent();
+}
