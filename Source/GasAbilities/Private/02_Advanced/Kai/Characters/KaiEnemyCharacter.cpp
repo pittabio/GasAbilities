f2c4ffd5cf8959178ab82c27899de2062db021e8
Fabio Pittaccio <pittacciofabio@gmail.com>
2025-11-14 12:34:00 +0100

    feat: initialize Enemy Character

---
+++ Source/GasAbilities/Private/02_Advanced/Kai/Characters/KaiEnemyCharacter.cpp
@@ -0,0 +1,22 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#include "02_Advanced/Kai/Characters/KaiEnemyCharacter.h"
+#include "02_Advanced/Kai/AbilitySystem/KaiAbilitySystemComponent.h"
+
+// Sets default values
+AKaiEnemyCharacter::AKaiEnemyCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UKaiAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+}
+
+// Called when the game starts or when spawned
+void AKaiEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+}
---
+++ Source/GasAbilities/Public/02_Advanced/Kai/Characters/KaiEnemyCharacter.h
@@ -0,0 +1,21 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "02_Advanced/Kai/Characters/KayCharacterBase.h"
+#include "KaiEnemyCharacter.generated.h"
+
+UCLASS()
+class GASABILITIES_API AKaiEnemyCharacter : public AKayCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AKaiEnemyCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
