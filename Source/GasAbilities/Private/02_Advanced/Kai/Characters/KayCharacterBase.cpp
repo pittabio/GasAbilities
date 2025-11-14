0186a29917f29b4b59919ee70dcc304649718345
Fabio Pittaccio <pittacciofabio@gmail.com>
2025-11-14 11:59:48 +0100

    feat: initialize the base Character

---
+++ Source/GasAbilities/Private/02_Advanced/Kai/Characters/KayCharacterBase.cpp
@@ -0,0 +1,16 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#include "02_Advanced/Kai/Characters/KayCharacterBase.h"
+#include "02_Advanced/Kai/AbilitySystem/KaiAbilitySystemComponent.h"
+
+// Sets default values
+AKayCharacterBase::AKayCharacterBase()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+UAbilitySystemComponent* AKayCharacterBase::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
---
+++ Source/GasAbilities/Public/02_Advanced/Kai/Characters/KayCharacterBase.h
@@ -0,0 +1,28 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "KayCharacterBase.generated.h"
+
+class UKaiAbilitySystemComponent;
+
+UCLASS()
+class GASABILITIES_API AKayCharacterBase : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AKayCharacterBase();
+	
+	// ~ IAbilitySystemInterface interface
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	// ~ End of IAbilitySystemInterface interface
+	
+protected:
+	UPROPERTY()
+	TObjectPtr<UKaiAbilitySystemComponent> AbilitySystemComponent;
+};
